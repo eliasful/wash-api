a9b4635ecd2e530183e11fd09b04f39ecd69f2b6
Elias <eliasful@gmail.com>
2019-05-23 01:39:17 -0300

    first commit

---
+++ api/controllers/ServicesQuantityController.js
@@ -0,0 +1,11 @@
+/**
+ * ServicesQuantityController
+ *
+ * @description :: Server-side logic for managing servicesquantities
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
---
+++ api/models/User.js
@@ -0,0 +1,56 @@
+/**
+ * User.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+      username: {
+        type: 'string',
+      },
+      email:{
+        type: 'string',
+      },
+      password: {
+        type: 'string',
+      },
+      name: {
+        type: 'string',
+      },
+      phone: {
+        type: 'string',
+      },
+      birthday: {
+        type: 'date',
+      },
+      document: {
+        type: 'string',
+      },
+
+      address: {
+        type: 'string',
+      },
+      number: {
+        type: 'string',
+      },
+      neighborhood: {
+        type: 'string',
+      },
+      city: {
+        type: 'string',
+      },
+      state: {
+        type: 'string',
+      },
+      lat: {
+        type: 'string',
+      },
+      lng: {
+        type: 'string',
+      }
+  }
+};
+
