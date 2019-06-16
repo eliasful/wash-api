b66134314e0ee4981b7cdfc560631c62675bbfb5
Elias <eliasful@gmail.com>
2019-06-16 18:28:48 -0300

    Implementa login

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -4,8 +4,36 @@
  * @description :: Server-side logic for managing users
  * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
  */
+const bcrypt = require('bcrypt');
 
 module.exports = {
-	
+  login(req, res) {
+    if (!req.body.email || !req.body.password) {
+      return res.badRequest({
+        err: "Email or password cannot be empty"
+      });
+    }
+
+    User.findOne({email: req.body.email}).exec(function (err, user) {
+      if (err) {
+        return res.serverError(err);
+      }
+
+      if (!user) {
+        return res.notFound({err: 'Could not find email,' + req.body.email + ' sorry.'});
+      }
+
+      bcrypt.compare(req.body.password, user.password, function (err, result) {
+        if (result) {
+          return res.json({
+            user: user,
+            token: jwToken.sign(user)
+          });
+        } else {
+          return res.forbidden({err: 'Email and password combination do not match'});
+        }
+      });
+    });
+  }
 };
 
--- api/models/User.js
+++ api/models/User.js
@@ -4,57 +4,64 @@
  * @description :: TODO: You might write a short summary of how this model works and what it represents here.
  * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
  */
+const bcrypt = require('bcrypt');
 
 module.exports = {
 
   attributes: {
-      username: {
-        type: 'string',
-      },
-      email:{
-        type: 'string',
-      },
-      password: {
-        type: 'string',
-      },
-      name: {
-        type: 'string',
-      },
-      phone: {
-        type: 'string',
-      },
-      birthday: {
-        type: 'date',
-      },
-      document: {
-        type: 'string',
-      },
+    email: {
+      type: 'string',
+    },
+    password: {
+      type: 'string',
+    },
+    type: {
+      type: 'string',
+    },
+    name: {
+      type: 'string',
+    },
+    phone: {
+      type: 'string',
+    },
+    birthday: {
+      type: 'date',
+    },
 
-      address: {
-        type: 'string',
-      },
-      number: {
-        type: 'string',
-      },
-      neighborhood: {
-        type: 'string',
-      },
-      city: {
-        type: 'string',
-      },
-      state: {
-        type: 'string',
-      },
-      lat: {
-        type: 'string',
-      },
-      lng: {
-        type: 'string',
-      },
-      userServices: {
-        collection: 'userService',
-        via: 'user'
-      }
+    address: {
+      type: 'string',
+    },
+    lat: {
+      type: 'string',
+    },
+    lng: {
+      type: 'string',
+    },
+    userServices: {
+      collection: 'userService',
+      via: 'user'
+    },
+
+    toJSON: function() {
+      const obj = this.toObject();
+      delete obj.password;
+
+      return obj;
+    }
+  },
+  beforeCreate(values, cb) {
+    if (!values.password) {
+      return cb({err: ["Password is required"]});
+    }
+
+    bcrypt.hash(values.password, 10, function (err, hash) {
+      if (err) {
+        return cb(err)
+      };
+      values.password = hash;
+
+      cb();
+    });
   }
 };
 
